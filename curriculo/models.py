cf36237d8c6105b1c5a9f2221565d8f3bd3c6128
Tiago <tiagolch@hotmail.com>
2020-07-07 23:47:14 -0300

    Primeiro commit Ambiente criado

---
+++ curriculo/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+"""
+classe link_social:
+    nome
+    
+classe pessoa:
+    nome completo
+    endereco
+    celular1
+    celular2
+    email
+    nome_link_social
+    
+classe projetos:
+    imagem
+    nome
+    descricao
+    link_projeto
+    link_codigo
+
+classe experiencia_profissional:
+    data_inicio
+    data_final
+    atual: boolean
+    empresa
+    cargo
+    descricao_funcao
+    
+classe formacao:
+    curso
+    data_inicio
+    data_final
+      
+clase depoimentos:
+    descricao
+    nome
+    cargo
+    empresa
+    
+"""
